072f689dd872955c1570f4fe4ebf71359c72aa1d
Vinayak V <vinayak.v@tecnotree.com>
2021-02-06 10:01:28 +0530

    added docker file

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const express = require('express');
 const app = express();
 
 
-const PORT = 3333;
+const PORT = 3000;
 
 app.get('/', (req, res)=>{
     res.send('<h1>my first Node App!</h1>');
+})
+
+
+app.get('/name', (req, res)=>{
+    res.send('its Vinayak');
 })
 
 
